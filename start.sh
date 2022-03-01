f8ab59a048ab68b0a42c28c27ed0a4f91e43a478
KaiTian0079 <178389386@qq.com>
2022-03-01 11:29:18 +0800

    修改代理IP池配置

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-python proxyPool.py server &
-python proxyPool.py schedule
+#删除进程
+kill -9 $(ps -ef | grep 'python proxyPool.py' | awk '{print $2}')
+
+#后台重新启动进程
+nohup python proxyPool.py server > /home/open_sources/proxy_pool/proxyPool-server.log 2>&1 & 
+nohup python proxyPool.py schedule > /home/open_sources/proxy_pool/proxyPool-schedule.log 2>&1 & 
